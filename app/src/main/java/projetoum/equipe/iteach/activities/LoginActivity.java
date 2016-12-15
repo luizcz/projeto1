48c1ac2894305fc81bf27f3ac0848e7234e08014
Isaque Fontinele <isaquefontinele@gmail.com>
2016-12-15 02:59:09 -0300

    Continuando o login

--- app/src/main/java/projetoum/equipe/iteach/activities/LoginActivity.java
+++ app/src/main/java/projetoum/equipe/iteach/activities/LoginActivity.java
@@ -1,15 +1,205 @@
 package projetoum.equipe.iteach.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Random;
 
 import projetoum.equipe.iteach.R;
+import projetoum.equipe.iteach.interfaces.ICallback;
+import projetoum.equipe.iteach.utils.DAO;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener{
+    private static final int RC_SIGN_IN = 0;
+
+    private GoogleApiClient mGoogleApiClient;
+//    private TextView mStatusTextView;
+//    private TextView feed;
+    private DAO dao;
+    private ICallback<Boolean> updateUI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        updateUI = new UpdateUI();
+        dao = DAO.getInstace(updateUI, this);
+
+        findViewById(R.id.bt_login_google).setOnClickListener(this);
+//        findViewById(R.id.sign_out_and_disconnect).setOnClickListener(this);
+//        findViewById(R.id.put).setOnClickListener(this);
+
+//        mStatusTextView = (TextView) findViewById(R.id.txt);
+//        feed = (TextView) findViewById(R.id.feed);
+
+
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+// options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+
     }
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        dao.addAuthStateListener();
+    }
+
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (dao.getAuthListener() != null) {
+            dao.removeAuthStateListener();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_login_google:
+                signIn();
+                break;
+//            case R.id.sign_out_and_disconnect:
+//                signOut();
+//                break;
+            case R.id.put:
+                dao.fillFeed();
+
+                break;
+        }
+    }
+
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d("Login Google", "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            Toast.makeText(this, acct.getDisplayName(), Toast.LENGTH_SHORT);
+//            mStatusTextView.setText(acct.getDisplayName());
+            dao.firebaseAuthWithGoogle(acct);
+            startActivity(new Intent(this, MainActivity.class));
+
+            //updateUI.execute(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            //updateUI.execute(false);
+        }
+    }
+
+
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "fail", Toast.LENGTH_SHORT);
+    }
+
+
+//    private void signOut() {
+//        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+//                new ResultCallback<Status>() {
+//                    @Override
+//                    public void onResult(Status status) {
+//                        updateUI.execute(false);
+//                        if (dao.getAuthListener() != null) {
+//                            dao.signOut();
+//                            // dao.removeAuthStateListener();
+//
+//                        }
+//                    }
+//                });
+//    }
+
+
+    public class UpdateUI implements ICallback<Boolean>{
+
+        @Override
+        public void execute(Boolean param) {
+
+
+            if (param) {
+                findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+//                findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+//                Toast.makeText(this, dao.getFireBaseUser().getDisplayName(), Toast.LENGTH_SHORT);
+//                mStatusTextView.setText(dao.getFireBaseUser().getDisplayName());
+//                dao.getFeed(new ICallback<String>() {
+//                    @Override
+//                    public void execute(String param) {
+//                        feed.setText(param);
+//                    }
+//                });
+            } else {
+//                Toast.makeText(this, "signed_out", Toast.LENGTH_SHORT);
+//                mStatusTextView.setText("signed_out");
+
+//                findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+//                findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+            }
+        }
+    };
 }
