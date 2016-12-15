383e18db72612af5e284f94bf7301539b67998da
Isaque Fontinele <isaquefontinele@gmail.com>
2016-12-15 16:46:00 -0300

    Login com o google working

--- app/src/main/java/projetoum/equipe/iteach/activities/LoginActivity.java
+++ app/src/main/java/projetoum/equipe/iteach/activities/LoginActivity.java
@@ -10,6 +10,17 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -22,6 +33,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -46,10 +58,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private DAO dao;
     private ICallback<Boolean> updateUI;
     private LoginActivity mContext;
+    private CallbackManager mCallbackManager;
+    private LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
 
         updateUI = new UpdateUI();
@@ -57,12 +72,35 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mContext = this;
 
         findViewById(R.id.bt_login_google).setOnClickListener(this);
+        findViewById(R.id.bt_login_face).setOnClickListener(this);
 //        findViewById(R.id.sign_out_and_disconnect).setOnClickListener(this);
 //        findViewById(R.id.put).setOnClickListener(this);
 
 //        mStatusTextView = (TextView) findViewById(R.id.txt);
 //        feed = (TextView) findViewById(R.id.feed);
 
+        mCallbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("Facebook", "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("Facebook", "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("Facebook", "facebook:onError", error);
+                // ...
+            }
+        });
 
 
         // Configure Google Sign In
@@ -73,14 +111,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
 
         // Build a GoogleApiClient with access to the Google Sign-In API and the
-// options specified by gso.
+        // options specified by gso.
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
-
-
-
     }
 
 
@@ -107,6 +142,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             case R.id.bt_login_google:
                 signIn();
                 break;
+            case R.id.bt_login_face:
+                loginButton.performClick();
+                break;
 //            case R.id.sign_out_and_disconnect:
 //                signOut();
 //                break;
@@ -117,7 +155,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
-
+    //Google methods start here
     private void signIn() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
@@ -154,16 +192,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
-
-
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Toast.makeText(this, "connection failed", Toast.LENGTH_SHORT);
-    }
-
-
-//    private void signOut() {
+    //    private void signOut() {
 //        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
 //                new ResultCallback<Status>() {
 //                    @Override
@@ -177,6 +206,42 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 //                    }
 //                });
 //    }
+
+    //Facebook methods start here
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d("FaceFirebase", "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        dao.firebaseAuthWithFacebook(credential);
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+
+    public void disconnectFromFacebook() {
+
+        if (AccessToken.getCurrentAccessToken() == null) {
+            return; // already logged out
+        }
+
+        new GraphRequest(AccessToken.getCurrentAccessToken(), "/me/permissions/", null, HttpMethod.DELETE, new GraphRequest
+                .Callback() {
+            @Override
+            public void onCompleted(GraphResponse graphResponse) {
+
+                LoginManager.getInstance().logOut();
+
+            }
+        }).executeAsync();
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "connection failed", Toast.LENGTH_SHORT);
+    }
+
+
+
 
 
     public class UpdateUI implements ICallback<Boolean>{
