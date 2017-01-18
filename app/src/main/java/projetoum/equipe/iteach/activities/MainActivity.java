2540f857c7574acb95e0072ad0ff2271635e8f27
luiz <luizha.cc@gmail.com>
2017-01-17 23:07:44 -0300

    View pesquisa

--- app/src/main/java/projetoum/equipe/iteach/activities/MainActivity.java
+++ app/src/main/java/projetoum/equipe/iteach/activities/MainActivity.java
@@ -1,11 +1,17 @@
 package projetoum.equipe.iteach.activities;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -61,6 +67,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ICallback<Boolean> updateUI;
     private CallbackManager mCallbackManager;
     private LoginButton loginButton;
+    private MenuItem menuSearch;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -161,6 +169,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 break;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        menuSearch = menu.findItem(R.id.search);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String string) {
+                //search_input = string;
+                //httpHandler.getBooks(20, selected_filter, string);
+                //new TimeOut().execute("1000");
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String string) {
+                if (!string.equals("")){
+                    //progressBar.setVisibility(View.VISIBLE);
+                }
+                //search_input = string;
+                //httpHandler.getBooks(20, selected_filter, string);
+                //new TimeOut().execute("1000");
+                return true;
+            }
+        });
+
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        MenuItemCompat.setOnActionExpandListener(menu.findItem(R.id.search), new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                //list_filters.setVisibility(View.INVISIBLE);
+                return true;  // Return true to collapse action view
+            }
+
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                return true;
+            }
+        });
+        return true;
     }
 
 
