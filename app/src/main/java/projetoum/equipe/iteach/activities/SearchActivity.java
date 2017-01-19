b1dfecdacb36024d636428d9f43f70fea37593cc
luiz <luizha.cc@gmail.com>
2017-01-18 21:10:16 -0300

    View pesquisa

---
+++ app/src/main/java/projetoum/equipe/iteach/activities/SearchActivity.java
@@ -0,0 +1,15 @@
+package projetoum.equipe.iteach.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import projetoum.equipe.iteach.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
