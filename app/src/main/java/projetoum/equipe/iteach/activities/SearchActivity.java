979c599f47f4ac70caa99a1ea37ff379b51e035d
Erik Sulpino <erik.sulpino.candeia@ccc.ufcg.edu.br>
2017-01-19 22:58:32 -0300

    Adicionando funcionalidade de busca por aulas

--- app/src/main/java/projetoum/equipe/iteach/activities/SearchActivity.java
+++ app/src/main/java/projetoum/equipe/iteach/activities/SearchActivity.java
@@ -6,21 +6,38 @@ import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Adapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 import projetoum.equipe.iteach.R;
+import projetoum.equipe.iteach.adapter.ClassAdapter;
+import projetoum.equipe.iteach.models.ClassObject;
 
 public class SearchActivity extends AppCompatActivity {
 
     private MenuItem menuSearch;
     private SearchView searchView;
+    private ClassAdapter searchAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        searchAdapter = new ClassAdapter(new ArrayList<ClassObject>(), this);
+        ListView searchListView = (ListView) findViewById(R.id.search_list);
+        searchListView.setAdapter(searchAdapter);
     }
 
     @Override
@@ -40,16 +57,42 @@ public class SearchActivity extends AppCompatActivity {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String string) {
-                //search_input = string;
-                //httpHandler.getBooks(20, selected_filter, string);
-                //new TimeOut().execute("1000");
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("class");
+                ref.orderByChild("name").startAt(string).addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        Log.e("Qualquercoisa", "qualquercoisa");
+                        ClassObject classObject = dataSnapshot.getValue(ClassObject.class);
+                        searchAdapter.addClass(classObject);
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
                 searchView.clearFocus();
                 return true;
             }
 
             @Override
             public boolean onQueryTextChange(String string) {
-                if (!string.equals("")){
+                if (!string.equals("")) {
                     //progressBar.setVisibility(View.VISIBLE);
                 }
                 //search_input = string;
